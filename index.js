19a4663992ba75b9fbbf39ff19d8c93b4a2dc562
Marcelo Eltz <celo.nh@gmail.com>
2019-07-31 23:51:18 -0300

    Finalizado aplicação.

---
+++ index.js
@@ -0,0 +1,83 @@
+const express = require("express");
+
+const server = express();
+
+server.use(express.json());
+
+let totalRequests = 0;
+const projects = [];
+
+function logRequests(req, res, next) {
+  totalRequests++;
+
+  console.log(`Total of requests: ${totalRequests}`);
+
+  return next();
+}
+
+function checkProject(req, res, next) {
+  const { id } = req.params;
+  const project = projects.find(p => p.id == id);
+
+  if (!project) {
+    return res.status(400).json({ error: "Project not found" });
+  }
+
+  return next();
+}
+
+server.use(logRequests);
+
+//ROUTES
+
+server.get("/projects", (req, res) => {
+  return res.json(projects);
+});
+
+server.post("/projects", (req, res) => {
+  const { id, title } = req.body;
+
+  const project = {
+    id,
+    title,
+    tasks: []
+  };
+
+  projects.push(project);
+
+  return res.json(project);
+});
+
+server.put("/projects/:id", checkProject, (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const project = projects.find(p => p.id == id);
+
+  project.title = title;
+
+  return res.json(project);
+});
+
+server.delete("/projects/:id", checkProject, (req, res) => {
+  const { id } = req.params;
+
+  const projectIndex = projects.findIndex(p => p.id == id);
+
+  projects.splice(projectIndex, 1);
+
+  return res.send();
+});
+
+server.post("/projects/:id/tasks", checkProject, (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const project = projects.find(p => p.id == id);
+
+  project.tasks.push(title);
+
+  return res.json(project);
+});
+
+server.listen(3000);
